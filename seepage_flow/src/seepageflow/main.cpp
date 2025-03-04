d25e854e1c74ae4f076f8d648dd2b8a2b92bc18f
raymondmg <wang.xu@ist.hokudai.ac.jp>
2025-03-04 19:06:57 +0900

    update

--- seepage_flow/src/seepageflow/main.cpp
+++ seepage_flow/src/seepageflow/main.cpp
@@ -1,12 +1,15 @@
-/*** 
- * @Author: Xu.WANG raymondmgwx@gmail.com
- * @Date: 2023-05-08 19:27:15
- * @LastEditors: Xu.WANG raymondmgwx@gmail.com
- * @LastEditTime: 2023-06-15 10:38:09
- * @FilePath: \sph_seepage_flows\seepage_flow\src\seepageflow\main.cpp
- * @Description: 
- * @Copyright (c) 2023 by Xu.WANG, All Rights Reserved. 
+/*
+ * File: main.cpp
+ * Module: seepageflow
+ * Created Date: 2025-03-04
+ * Author: Xu WANG
+ * -----
+ * Last Modified: 2025-03-04
+ * Modified By: Xu WANG
+ * -----
+ * Copyright (c) 2025 Xu WANG
  */
+
 // clang-format off
 #include <sf_cuda_define.h>
 #include <kiri_utils.h>
@@ -19,7 +22,7 @@ using namespace KIRI;
 auto ExampleName = "seepageflow_bunny_wcsph";
 
 auto RunLiquidNumber = 0;
-auto TotalFrameNumber = 120;
+auto TotalFrameNumber = 50;
 auto SimCount = 0;
 auto TotalFrameTime = 0.f;
 auto RenderInterval = 1.f / 60.f;
@@ -177,7 +180,8 @@ void Seepage_UniBunny_WCSPH() {
     auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
     volumeEmitter->BuildSeepageflowShapeMultiVolume(
-        multi_volume_data, sand_shape, CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
         CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
         CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(1.2f, 0.6f));
 
@@ -202,8 +206,8 @@ void Seepage_UniBunny_WCSPH() {
   particles = std::make_shared<CudaSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -223,8 +227,6 @@ void Seepage_UniBunny_WCSPH() {
   pSolver = std::make_shared<CudaWCSphSFSolver>(particles->MaxSize());
   CUDA_SEEPAGEFLOW_PARAMS.solver_type = WCSPH_SOLVER;
   KIRI_LOG_INFO("Current Fluid Solver= WCSPH");
-
-
 
   CudaGNBoundarySearcherPtr boundary_searcher =
       std::make_shared<CudaGNBoundarySearcher>(
@@ -386,7 +388,8 @@ void Seepage_MSBunny_WCSPH() {
     auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
     volumeEmitter->BuildSeepageflowShapeMultiVolume(
-        multi_volume_data, sand_shape, CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
         CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
         CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(1.2f, 0.6f));
 
@@ -411,8 +414,8 @@ void Seepage_MSBunny_WCSPH() {
   particles = std::make_shared<CudaSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -432,8 +435,6 @@ void Seepage_MSBunny_WCSPH() {
   pSolver = std::make_shared<CudaWCSphSFSolver>(particles->MaxSize());
   CUDA_SEEPAGEFLOW_PARAMS.solver_type = WCSPH_SOLVER;
   KIRI_LOG_INFO("Current Fluid Solver= WCSPH");
-
-
 
   CudaGNBoundarySearcherPtr boundary_searcher =
       std::make_shared<CudaGNBoundarySearcher>(
@@ -595,7 +596,8 @@ void Seepage_UniBunny_DFSPH() {
     auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
     volumeEmitter->BuildSeepageflowShapeMultiVolume(
-        multi_volume_data, sand_shape, CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
         CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
         CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(1.2f, 0.6f));
 
@@ -621,8 +623,8 @@ void Seepage_UniBunny_DFSPH() {
   particles = std::make_shared<CudaDFSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -643,8 +645,6 @@ void Seepage_UniBunny_DFSPH() {
                                                 CUDA_SEEPAGEFLOW_PARAMS.dt);
   CUDA_SEEPAGEFLOW_PARAMS.solver_type = DFSPH_SOLVER;
   KIRI_LOG_INFO("Current Fluid Solver= DFSPH");
-
-
 
   CudaGNBoundarySearcherPtr boundary_searcher =
       std::make_shared<CudaGNBoundarySearcher>(
@@ -806,7 +806,8 @@ void Seepage_MSBunny_DFSPH() {
     auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
     volumeEmitter->BuildSeepageflowShapeMultiVolume(
-        multi_volume_data, sand_shape, CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
         CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
         CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(1.2f, 0.6f));
 
@@ -832,8 +833,8 @@ void Seepage_MSBunny_DFSPH() {
   particles = std::make_shared<CudaDFSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -855,8 +856,6 @@ void Seepage_MSBunny_DFSPH() {
   CUDA_SEEPAGEFLOW_PARAMS.solver_type = DFSPH_SOLVER;
   KIRI_LOG_INFO("Current Fluid Solver= DFSPH");
 
-
-
   CudaGNBoundarySearcherPtr boundary_searcher =
       std::make_shared<CudaGNBoundarySearcher>(
           CUDA_BOUNDARY_PARAMS.lowest_point, CUDA_BOUNDARY_PARAMS.highest_point,
@@ -875,7 +874,7 @@ void Seepage_MSDam_WCSPH() {
   strcpy(CUDA_SEEPAGEFLOW_APP_PARAMS.bgeo_export_folder,
          (String(EXPORT_PATH) + "bgeo/" + ExampleName).c_str());
 
- // scene config
+  // scene config
   auto cuda_lowest_point = make_float3(0.f);
   auto cuda_highest_point = make_float3(1.65f, 2.f, 3.f);
   auto cuda_world_size = cuda_highest_point - cuda_lowest_point;
@@ -913,7 +912,6 @@ void Seepage_MSDam_WCSPH() {
   CUDA_SEEPAGEFLOW_PARAMS.dem_tan_friction_angle = 0.5f;
   CUDA_SEEPAGEFLOW_PARAMS.dem_damping = 0.4f;
 
-
   CUDA_SEEPAGEFLOW_PARAMS.sf_c0 = 2.f;
   CUDA_SEEPAGEFLOW_PARAMS.sf_cd = 0.01f;
   CUDA_SEEPAGEFLOW_PARAMS.sf_csat = 0.f;
@@ -935,9 +933,9 @@ void Seepage_MSDam_WCSPH() {
   // sph emitter
   CUDA_SPH_EMITTER_PARAMS.enable = true;
   CUDA_SPH_EMITTER_PARAMS.run = false;
-  CUDA_SPH_EMITTER_PARAMS.emit_pos =
-      make_float3(cuda_world_center.x, cuda_world_center.y+cuda_world_center.y/4.f,
-                  cuda_world_center.z + cuda_world_size.z / 2.5f);
+  CUDA_SPH_EMITTER_PARAMS.emit_pos = make_float3(
+      cuda_world_center.x, cuda_world_center.y + cuda_world_center.y / 4.f,
+      cuda_world_center.z + cuda_world_size.z / 2.5f);
   CUDA_SPH_EMITTER_PARAMS.emit_vel = make_float3(0.f, -5.f, 0.f);
   CUDA_SPH_EMITTER_PARAMS.emit_col = make_float3(127.f, 205.f, 255.f) / 255.f;
 
@@ -987,7 +985,7 @@ void Seepage_MSDam_WCSPH() {
 
   // object 1: bunny/bunny.bego
   shape_folders.emplace_back("dam");
-  shape_files.emplace_back("dam_xprotosphere");
+  shape_files.emplace_back("dam");
 
   std::vector<float3> cd_a0_asat;
   std::vector<float2> amc_amcp;
@@ -999,22 +997,21 @@ void Seepage_MSDam_WCSPH() {
                                     CUDA_SEEPAGEFLOW_PARAMS.sf_amc_p));
 
   multi_volume_data.min_radius = CUDA_SEEPAGEFLOW_PARAMS.sph_particle_radius;
-    for (auto i = 0; i < shape_folders.size(); i++) {
-      auto cda0asat = cd_a0_asat[i];
-      auto amcamcp = amc_amcp[i];
-      auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
+  for (auto i = 0; i < shape_folders.size(); i++) {
+    auto cda0asat = cd_a0_asat[i];
+    auto amcamcp = amc_amcp[i];
+    auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
-      volumeEmitter->BuildSeepageflowShapeMultiVolume(
-          multi_volume_data, sand_shape,
-          CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
-          CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp,
-          offset2Ground, CUDA_BOUNDARY_PARAMS.lowest_point.y,
-          make_float2(0.83f, 1.5f));
+    volumeEmitter->BuildSeepageflowShapeMultiVolume(
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
+        CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(0.83f, 1.5f));
 
-      KIRI_LOG_DEBUG(
-          "Object({0}) Params: Cd A0 Asat Amc Amcp = {1}, {2}, {3}, {4}, {5}", i + 1, cda0asat.x, cda0asat.y, cda0asat.z, amcamcp.x,
-          amcamcp.y);
-    }
+    KIRI_LOG_DEBUG(
+        "Object({0}) Params: Cd A0 Asat Amc Amcp = {1}, {2}, {3}, {4}, {5}",
+        i + 1, cda0asat.x, cda0asat.y, cda0asat.z, amcamcp.x, amcamcp.y);
+  }
 
   // dt
   CUDA_SEEPAGEFLOW_PARAMS.dt = 0.5f * multi_volume_data.min_radius /
@@ -1032,8 +1029,8 @@ void Seepage_MSDam_WCSPH() {
   particles = std::make_shared<CudaSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -1054,8 +1051,6 @@ void Seepage_MSDam_WCSPH() {
   CUDA_SEEPAGEFLOW_PARAMS.solver_type = WCSPH_SOLVER;
   KIRI_LOG_INFO("Current Fluid Solver= WCSPH");
 
-
-
   CudaGNBoundarySearcherPtr boundary_searcher =
       std::make_shared<CudaGNBoundarySearcher>(
           CUDA_BOUNDARY_PARAMS.lowest_point, CUDA_BOUNDARY_PARAMS.highest_point,
@@ -1065,7 +1060,6 @@ void Seepage_MSDam_WCSPH() {
       particles, boundary_particles, pSolver, searcher, boundary_searcher,
       emitter, adaptive_sub_timestep);
 }
-
 
 void Seepage_MSDam_DFSPH() {
 
@@ -1113,7 +1107,6 @@ void Seepage_MSDam_DFSPH() {
   CUDA_SEEPAGEFLOW_PARAMS.dem_tan_friction_angle = std::tanf(0.5f);
   CUDA_SEEPAGEFLOW_PARAMS.dem_damping = 0.4f;
 
-
   CUDA_SEEPAGEFLOW_PARAMS.sf_c0 = 2.f;
   CUDA_SEEPAGEFLOW_PARAMS.sf_cd = 0.015f;
   CUDA_SEEPAGEFLOW_PARAMS.sf_csat = 0.f;
@@ -1135,9 +1128,9 @@ void Seepage_MSDam_DFSPH() {
   // sph emitter
   CUDA_SPH_EMITTER_PARAMS.enable = true;
   CUDA_SPH_EMITTER_PARAMS.run = false;
-  CUDA_SPH_EMITTER_PARAMS.emit_pos =
-      make_float3(cuda_world_center.x, cuda_world_center.y+cuda_world_center.y/4.f,
-                  cuda_world_center.z + cuda_world_size.z / 2.5f);
+  CUDA_SPH_EMITTER_PARAMS.emit_pos = make_float3(
+      cuda_world_center.x, cuda_world_center.y + cuda_world_center.y / 4.f,
+      cuda_world_center.z + cuda_world_size.z / 2.5f);
   CUDA_SPH_EMITTER_PARAMS.emit_vel = make_float3(0.f, -5.f, 0.f);
   CUDA_SPH_EMITTER_PARAMS.emit_col = make_float3(127.f, 205.f, 255.f) / 255.f;
 
@@ -1200,22 +1193,21 @@ void Seepage_MSDam_DFSPH() {
                                     CUDA_SEEPAGEFLOW_PARAMS.sf_amc_p));
 
   multi_volume_data.min_radius = CUDA_SEEPAGEFLOW_PARAMS.sph_particle_radius;
-    for (auto i = 0; i < shape_folders.size(); i++) {
-      auto cda0asat = cd_a0_asat[i];
-      auto amcamcp = amc_amcp[i];
-      auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
+  for (auto i = 0; i < shape_folders.size(); i++) {
+    auto cda0asat = cd_a0_asat[i];
+    auto amcamcp = amc_amcp[i];
+    auto sand_shape = ReadBgeoFileForGPU(shape_folders[i], shape_files[i]);
 
-      volumeEmitter->BuildSeepageflowShapeMultiVolume(
-          multi_volume_data, sand_shape,
-          CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
-          CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp,
-          offset2Ground, CUDA_BOUNDARY_PARAMS.lowest_point.y,
-          make_float2(0.83f, 1.5f));
+    volumeEmitter->BuildSeepageflowShapeMultiVolume(
+        multi_volume_data, sand_shape,
+        CUDA_SEEPAGEFLOW_PARAMS.sf_dry_sand_color,
+        CUDA_SEEPAGEFLOW_PARAMS.dem_density, cda0asat, amcamcp, offset2Ground,
+        CUDA_BOUNDARY_PARAMS.lowest_point.y, make_float2(0.83f, 1.5f));
 
-      KIRI_LOG_DEBUG(
-          "Object({0}) Params: Cd A0 Asat Amc Amcp = {1}, {2}, {3}, {4}, {5}", i + 1, cda0asat.x, cda0asat.y, cda0asat.z, amcamcp.x,
-          amcamcp.y);
-    }
+    KIRI_LOG_DEBUG(
+        "Object({0}) Params: Cd A0 Asat Amc Amcp = {1}, {2}, {3}, {4}, {5}",
+        i + 1, cda0asat.x, cda0asat.y, cda0asat.z, amcamcp.x, amcamcp.y);
+  }
 
   // dt
   CUDA_SEEPAGEFLOW_PARAMS.dt = 0.5f * multi_volume_data.min_radius /
@@ -1233,8 +1225,8 @@ void Seepage_MSDam_DFSPH() {
   particles = std::make_shared<CudaDFSFParticles>(
       CUDA_SEEPAGEFLOW_APP_PARAMS.max_num, multi_volume_data.pos,
       multi_volume_data.col, multi_volume_data.label, multi_volume_data.mass,
-      multi_volume_data.inertia, multi_volume_data.radius, multi_volume_data.cda0asat,
-      multi_volume_data.amcamcp);
+      multi_volume_data.inertia, multi_volume_data.radius,
+      multi_volume_data.cda0asat, multi_volume_data.amcamcp);
 
   CudaGNSearcherPtr searcher;
   searcher = std::make_shared<CudaGNSearcher>(
@@ -1266,22 +1258,20 @@ void Seepage_MSDam_DFSPH() {
       emitter, adaptive_sub_timestep);
 }
 
-
 void main() {
   KiriLog::Init();
 
- //Seepage_UniBunny_WCSPH();
+  // Seepage_UniBunny_WCSPH();
 
- //Seepage_MSBunny_WCSPH();
+  // Seepage_MSBunny_WCSPH();
 
- // Seepage_UniBunny_DFSPH();
+  // Seepage_UniBunny_DFSPH();
 
- //Seepage_MSBunny_DFSPH();
+  // Seepage_MSBunny_DFSPH();
 
- Seepage_MSDam_WCSPH();
+  Seepage_MSDam_WCSPH();
 
- //Seepage_MSDam_DFSPH();
-
+  // Seepage_MSDam_DFSPH();
 
   // abc exporter params
   auto AbcDtScale = 120.f * RenderInterval;
@@ -1327,25 +1317,25 @@ void main() {
       TotalFrameTime += PerFrameTimer.Elapsed();
 
       // export
-        auto particles = SFSystem->GetSFParticles();
-        // ExportBgeoFileCUDA(
-        //     CUDA_SEEPAGEFLOW_APP_PARAMS.bgeo_export_folder,
-        //     UInt2Str4Digit(SimCount - RunLiquidNumber),
-        //     particles->GetPosPtr(),
-        //     particles->GetColPtr(),
-        //     particles->GetRadiusPtr(),
-        //     particles->GetLabelPtr(),
-        //     particles->Size());
+      auto particles = SFSystem->GetSFParticles();
+      // ExportBgeoFileCUDA(
+      //     CUDA_SEEPAGEFLOW_APP_PARAMS.bgeo_export_folder,
+      //     UInt2Str4Digit(SimCount - RunLiquidNumber),
+      //     particles->GetPosPtr(),
+      //     particles->GetColPtr(),
+      //     particles->GetRadiusPtr(),
+      //     particles->GetLabelPtr(),
+      //     particles->Size());
 
-        AbcPosData->SubmitCurrentStatusFloat3(particles->GetPosPtr(),
+      AbcPosData->SubmitCurrentStatusFloat3(particles->GetPosPtr(),
+                                            particles->Size());
+
+      AbcScaleData->SubmitCurrentStatusFloat(particles->GetRadiusPtr(),
+                                             particles->Size());
+
+      AbcColorData->SubmitCurrentStatusFloat3(particles->GetColPtr(),
                                               particles->Size());
 
-        AbcScaleData->SubmitCurrentStatusFloat(particles->GetRadiusPtr(),
-                                               particles->Size());
-
-        AbcColorData->SubmitCurrentStatusFloat3(particles->GetColPtr(),
-                                                particles->Size());
-      
     } else if (CUDA_SEEPAGEFLOW_APP_PARAMS.run) {
       CUDA_SEEPAGEFLOW_APP_PARAMS.run = false;
 
